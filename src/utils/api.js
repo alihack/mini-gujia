693bc3013fcdf19febced66289b3a3721bcfa006
陈立 <chenli@chenlideMacBook-Air.local>
2018-11-27 17:21:29 +0800

    canvas

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 const roots = {
 	prod: 'https://love.yylhealth.com/',
-	test: 'http://118.25.21.169:3338/',
-	dev: 'https://hd-api.dev.xiaoyu.com/index.php/wxapp/fngj/index/'
+	dev: 'http://192.168.1.46/index.php/wxapp/fngj/index/',
+	test: 'https://hd-api.dev.xiaoyu.com/index.php/wxapp/fngj/index/'
 }
 const apis = [
 	'getUserId',
@@ -13,10 +13,11 @@ const apis = [
 	'addHouseInfo',
 	'sendCode',
 	'mobileLogin',
-	'addUserIntention'
+	'addUserIntention',
+	'getUserMobile'
 ]
 
-const env = 'dev' // dev: 本地测试服务器， prod：线上服务器, test：测试服务器
+const env = 'test' // dev: 本地测试服务器， prod：线上服务器, test：测试服务器
 const root = roots[env]
 const api = {}
 apis.forEach(ele => {
--- src/utils/util.js
+++ src/utils/util.js
@@ -1,7 +1,7 @@
 /* eslint-disable yoda */
 import wepy from 'wepy'
 import api from './api'
-import QQMapWX from './qqmap-wx-jssdk.js'
+// import QQMapWX from './qqmap-wx-jssdk.js'
 
 const getUserId = () => {
 	return new Promise(async resolve => {
@@ -10,7 +10,7 @@ const getUserId = () => {
 		// 有uid则不重新获取
 		if (uid) {
 			console.log('有uid不重新获取')
-			resolve()
+			resolve(uid)
 			return
 		}
 		wx.login({
@@ -23,10 +23,12 @@ const getUserId = () => {
 							code: res.code
 						}
 					})
-					if (!data.uid) wepy.redirectTo({url: 'loginByAuthor'})
-					console.log(data)
-					wx.setStorageSync('uid', data.uid)
-					resolve()
+					if (!data.uid) {
+						resolve('')
+					} else {
+						wx.setStorageSync('uid', data.uid)
+						resolve(data.uid)
+					}
 				} else {
 					console.log('登录失败！' + res.errMsg)
 				}
@@ -41,7 +43,7 @@ const getUserInfo = () => {
 		const uid = wx.getStorageSync('uid')
 		// 有myUserInfo则不重新获取
 		if (wepy.$instance.globalData.myUserInfo || !uid) {
-			console.log('有myUserInfo不重新获取')
+			console.log('有myUserInfo或没有uid不重新获取')
 			resolve()
 			return
 		}
@@ -110,64 +112,71 @@ const dataController = (ele) => {
 		ele.unit_price = parseFloat(ele.unit_price)
 		ele.acreage = parseFloat(ele.acreage)
 		// 房源分析
-		let sourceAnalyze
-		switch (ele.orientation) {
-			case '南':
-				sourceAnalyze = '采光充足，通风好，冬暖夏凉，但是相对均价较高。'
+		let sourceAnalyze = [{}, {}, {}]
+			// 面积
+		const acreage = parseFloat(ele.acreage)
+		sourceAnalyze[0].txt1 = ele.house_type
+		sourceAnalyze[0].txt2 = `${ele.acreage}m`
+		switch (true) {
+			case 0 <= acreage && acreage <= 49:
+				sourceAnalyze[0].content = '该房屋适合单身人士，装修居住成本较低，为缺乏资金的年轻人的首选。'
 				break
-			case '东南':
-				sourceAnalyze = '采光充足，冬季暖和，但通风略显不足，所以容易潮湿。'
+			case 50 <= acreage && acreage <= 89:
+				sourceAnalyze[0].content = '作为过渡型的小户型，适合资金不足的首次置业人群以及刚需'
 				break
-			case '东':
-				sourceAnalyze = '上午阳光充足，采光较好，通风相对较差。'
+			case 90 <= acreage && acreage <= 139:
+				sourceAnalyze[0].content = '该户型属于上面上的主流户型，作为市场刚需产品，保值能力较强。'
 				break
-			case '西南':
-				sourceAnalyze = '除了早晨之外，其余采光时间较好，但四季通风都较差，且有西晒，冬季偏冷。'
-				break
-			case '西':
-				sourceAnalyze = '通风较好，但夏季西晒严重。'
-				break
-			case '东北':
-				sourceAnalyze = '早晨会有阳光，冬季会有些寒冷。'
-				break
-			case '西北':
-				sourceAnalyze = '均价比较低，但需要忍受西晒，大部分的房间都没有阳光的照射。'
-				break
-			case '北':
-				sourceAnalyze = '夏季凉爽，在采光上具有劣势，且冬季阴冷。'
+			case 140 <= acreage:
+				sourceAnalyze[0].content = '该房屋属于改善类大户型，居住惬意，适合资金充裕的人群。'
 				break
 		}
 		const storeyNum = parseFloat(ele.storey.slice(0, ele.storey.indexOf('层')))
+		sourceAnalyze[1].txt1 = '楼层'
+		sourceAnalyze[1].txt2 = ele.storey.replace(/共/, '/')
 		switch (true) {
 			case 1 <= storeyNum && storeyNum <= 4:
-				sourceAnalyze += '低楼层出行更方便，适合有老人、儿童的家庭。但通风、采光较差，噪音较大且易受潮。'
+				sourceAnalyze[1].content = '低楼层出行更方便，适合有老人、儿童的家庭。但通风、采光较差，噪音较大且易受潮。'
 				break
 			case 5 <= storeyNum && storeyNum <= 8:
-				sourceAnalyze += '黄金楼层，兼顾出行与采光通风，居住品质较高，视野较好。'
+				sourceAnalyze[1].content = '黄金楼层，兼顾出行与采光通风，居住品质较高，视野较好。'
 				break
 			case 9 <= storeyNum && storeyNum <= 11:
-				sourceAnalyze += '俗称扬灰层，空气中灰尘含量较高，但视野通风良好。'
+				sourceAnalyze[1].content = '俗称扬灰层，空气中灰尘含量较高，但视野通风良好。'
 				break
 			case 12 <= storeyNum && storeyNum <= 18:
-				sourceAnalyze += '采光优秀，通风、视野良好，居住质量较高，但老人出行或有不便。'
+				sourceAnalyze[1].content = '采光优秀，通风、视野良好，居住质量较高，但老人出行或有不便。'
 				break
 			case 19 <= storeyNum:
-				sourceAnalyze += '视野绝佳，空气清新，但风力较大，或许存在冬冷夏热的可能。'
+				sourceAnalyze[1].content = '视野绝佳，空气清新，但风力较大，或许存在冬冷夏热的可能。'
 				break
 		}
-		const acreage = parseFloat(ele.acreage)
-		switch (true) {
-			case 0 <= acreage && acreage <= 49:
-				sourceAnalyze += '该房屋适合单身人士，装修居住成本较低，为缺乏资金的年轻人的首选。'
+		sourceAnalyze[2].txt1 = '朝向'
+		sourceAnalyze[2].txt2 = ele.orientation
+		switch (ele.orientation) {
+			case '南':
+				sourceAnalyze[2].content = '采光充足，通风好，冬暖夏凉，但是相对均价较高。'
 				break
-			case 50 <= acreage && acreage <= 89:
-				sourceAnalyze += '作为过渡型的小户型，适合资金不足的首次置业人群以及刚需'
+			case '东南':
+				sourceAnalyze[2].content = '采光充足，冬季暖和，但通风略显不足，所以容易潮湿。'
 				break
-			case 90 <= acreage && acreage <= 139:
-				sourceAnalyze += '该户型属于上面上的主流户型，作为市场刚需产品，保值能力较强。'
+			case '东':
+				sourceAnalyze[2].content = '上午阳光充足，采光较好，通风相对较差。'
 				break
-			case 140 <= acreage:
-				sourceAnalyze += '该房屋属于改善类大户型，居住惬意，适合资金充裕的人群。'
+			case '西南':
+				sourceAnalyze[2].content = '除了早晨之外，其余采光时间较好，但四季通风都较差，且有西晒，冬季偏冷。'
+				break
+			case '西':
+				sourceAnalyze[2].content = '通风较好，但夏季西晒严重。'
+				break
+			case '东北':
+				sourceAnalyze[2].content = '早晨会有阳光，冬季会有些寒冷。'
+				break
+			case '西北':
+				sourceAnalyze[2].content = '均价比较低，但需要忍受西晒，大部分的房间都没有阳光的照射。'
+				break
+			case '北':
+				sourceAnalyze[2].content = '夏季凉爽，在采光上具有劣势，且冬季阴冷。'
 				break
 		}
 		ele.sourceAnalyze = sourceAnalyze
